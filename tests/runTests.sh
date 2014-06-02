9bfbc7cdf6c52134b30c007ddb5f6c3b2d587bfa
Nathan Braswell <miloignis@gmail.com>
2014-06-02 00:19:44 -0700

    Script to run all tests added. Prep work on new computer before fixing the remaining major template class bug.

---
+++ tests/runTests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+krakenPath="../build/kraken"
+testDir=${1:-"../tests"}
+ext=${2:-"krak"}
+
+fileList=""
+for dir in `find ${testDir} -type f -name "*.${ext}"`; do
+	filename=$(basename ${dir})
+	filename="${filename%.*}"
+	fileList+=\ \.\/$filename
+done
+
+${krakenPath} "--test" ${fileList}
