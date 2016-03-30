09158cd2b9129e353c5542d692bb2fe2d4d213f0
Nathan Braswell <miloignis@gmail.com>
2016-03-30 16:13:10 -0400

    Fix vector out of bounds access for parser, ast_transformation, and c_generator, remove many printlns in prep for better error handling, add features to captian.sh to facillitate development

--- captain.sh
+++ captain.sh
@@ -2,41 +2,64 @@
 
 kraken="kraken"
 
-if [ -s "$kraken" ]
+if [[ $1 == "clean" ]]
 then
-    #echo "$kraken exists, calling"
-    echo "$kraken exists!"
+    rm ${kraken}
+    rm ${kraken}_bac
+    rm ${kraken}_deprecated
+    rm -r deprecated_compiler/stdlib
+    rm deprecated_compiler/krakenGrammer.kgm.comp
+    rm deprecated_compiler/krakenGrammer.kgm
+    rm -r deprecated_compiler/build
+    rm -r deprecated_compiler/build_kraken
 else
-    echo "gotta make $kraken, testing for compilers to do so"
-    if ! [ -s "${kraken}_bac" ]
+    if [[ $1 == "backup" ]]
     then
-        if ! [ -s "${kraken}_deprecated" ]
-        then
-            echo "no ${kraken}_deprecated, using Cephelpod"
-            cp -r stdlib deprecated_compiler
-            cp krakenGrammer.kgm deprecated_compiler
-            cp kraken.krak deprecated_compiler
-            pushd deprecated_compiler
-            mkdir build
-            pushd build
-            cmake ..
-            make
-            popd
-            mkdir build_kraken
-            mv kraken.krak build_kraken
-            pushd build_kraken
-            ../build/kraken kraken.krak
-            popd
-            popd
-            cp deprecated_compiler/build_kraken/kraken/kraken ./kraken_deprecated
-        else
-            echo "${kraken}_deprecated exists, calling"
-        fi
-        ./kraken_deprecated kraken.krak kraken_bac
-    else
-        echo "${kraken}_bac exists, calling"
+        rm ${kraken}
     fi
-    ./${kraken}_bac ${kraken}.krak
+    if [[ $1 == "rebuild" ]]
+    then
+        rm ${kraken}
+        rm ${kraken}_bac
+        rm ${kraken}_deprecated
+    fi
+
+    if [ -s "$kraken" ]
+    then
+        #echo "$kraken exists, calling"
+        ./${kraken} ${kraken}.krak ${kraken}
+    else
+        echo "gotta make $kraken, testing for compilers to do so"
+        if ! [ -s "${kraken}_bac" ]
+        then
+            if ! [ -s "${kraken}_deprecated" ]
+            then
+                echo "no ${kraken}_deprecated, using Cephelpod"
+                cp -r stdlib deprecated_compiler
+                cp krakenGrammer.kgm deprecated_compiler
+                cp kraken.krak deprecated_compiler
+                pushd deprecated_compiler
+                mkdir build
+                pushd build
+                cmake ..
+                make
+                popd
+                mkdir build_kraken
+                mv kraken.krak build_kraken
+                pushd build_kraken
+                ../build/kraken kraken.krak
+                popd
+                popd
+                cp deprecated_compiler/build_kraken/kraken/kraken ./${kraken}_deprecated
+            else
+                echo "${kraken}_deprecated exists, calling"
+            fi
+            ./${kraken}_deprecated kraken.krak ${kraken}_bac
+        else
+            echo "${kraken}_bac exists, calling"
+        fi
+        ./${kraken}_bac kraken.krak ${kraken}
+    fi
 fi
 
 #./${kraken} $@
