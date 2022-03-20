eb3a732f99f1520d2855029e47a605c4ee389d8c
Nathan Braswell <miloignis@gmail.com>
2022-03-20 16:01:38 -0400

    Add fib_let test and script to run all of them. Indeed, the version with the let is worse when interpreted and the same/better when partially-evaled and compiled

---
+++ fib_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+NUMBER=11
+
+echo "Compile Straight"
+touch csc_out.wasm && rm csc_out.wasm && scheme --script ./partial_eval.scm fib.kp                && time echo $NUMBER | wasm3 ./csc_out.wasm
+
+echo "Interpret Straight"
+touch csc_out.wasm && rm csc_out.wasm && scheme --script ./partial_eval.scm fib.kp no_compile     && time echo $NUMBER | wasm3 ./csc_out.wasm
+
+echo "Compile Let"
+touch csc_out.wasm && rm csc_out.wasm && scheme --script ./partial_eval.scm fib_let.kp            && time echo $NUMBER | wasm3 ./csc_out.wasm
+
+echo "Interpret Let"
+touch csc_out.wasm && rm csc_out.wasm && scheme --script ./partial_eval.scm fib_let.kp no_compile && time echo $NUMBER | wasm3 ./csc_out.wasm
+
+
